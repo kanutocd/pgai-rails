a767689d0df3aea57cf74cef0e5b518d24450445
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2025-08-09 01:23:10 +0800

    chore: add requires in the main entrypoint

--- lib/pgai_rails.rb
+++ lib/pgai_rails.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
 require_relative 'pgai_rails/version'
+require_relative 'pgai_rails/configuration'
+require_relative 'pgai_rails/errors'
+require_relative 'pgai_rails/provider'
+require_relative 'pgai_rails/extension_checker'
+require_relative 'pgai_rails/vectorizer_builder'
+require_relative 'pgai_rails/migration_helpers'
 
 module PgaiRails
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+  end
 end
+
+require_relative 'pgai_rails/railtie' if defined?(Rails::Railtie)
